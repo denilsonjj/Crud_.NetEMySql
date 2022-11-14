183761d34f9079af676a2df1d4699c06b584cacc
Denilson junior <juniordenilson363@gmail.com>
2022-11-14 15:03:47 -0300

    Primeiro controller e testes nas páginas Razor

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
+            ViewData["email"] = "juniordenilson363@gmaol.com";
             return View();
         }
 
